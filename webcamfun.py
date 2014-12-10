757800aea918468c96dfb75eb8688af56dd769d6
Liam Sargent <themailman05@gmail.com>
2014-12-10 16:14:05 -0500

    Fixed live drawing of rectangles on faces

--- webcamfun.py
+++ webcamfun.py
@@ -24,44 +24,40 @@ while True:
         flags=cv2.cv.CV_HAAR_SCALE_IMAGE
     )
 
-# Detect Eyes in the image
-eyes = eyeCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.2,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-)
+    # Detect Eyes in the image
+    eyes = eyeCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
 
-# Detect Hands in the image
-hands = handCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.2,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-)
+    # Detect Hands in the image
+    hands = handCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
 
-   # Draw a rectangle around the faces
-for (x, y, w, h) in faces:
- {
-  cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    # Draw a rectangle around the eyes
-  for (x, y, w, h) in eyes:
-  {
-    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+       # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # Draw a rectangle around the eyes
+    for (x, y, w, h) in eyes:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
     for (x, y, w, h) in hands:
-      cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
-  }
- }
 
 
     # Display the resulting frame
-  cv2.imshow('Video', frame)
+    cv2.imshow('Video', frame)
 
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-    break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # When everything is done, release the capture
 video_capture.release()
